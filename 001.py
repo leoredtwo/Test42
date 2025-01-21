92cd1f92cb2601a3c29c3619d8062d81e7e0ddd9
leoredtwo <Egav@yandex.ru>
2025-01-21 21:52:30 +0300

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,1 +1,10 @@
-print ("clasical_words "hello world"")
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+            }
+word = input("Введите непонятное слово (большими буквами!): ")
+if word in meme_dict.keys():
+    meme_dict[word]
+    print (meme_dict[word])
+else:
+   print ("этого слова у нас нет в базе данных, пожалуйста обратитесь вот по этой сылке : https://youtu.be/ech4bJ5g9V8?si=91Mg0XIAp8jDNtV2")
